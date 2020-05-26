7165390ab2784862bd4f6ba20bbd118b33a7c3a2
Daniel Cancado Caram Andre <dandre9@gmail.com>
2020-05-26 16:59:01 -0300

    feat: Insert TouchableOpacity to each deck on the list so user can select a deck

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function DeckListView() {
+export default function DeckListView({ decks, selectDeck }) {
   return (
-    <View>
-      <Text>DeckListView</Text>
+    <View style={styles.container}>
+      {Object.keys(decks).map((deck) => (
+        <TouchableOpacity
+          onPress={() => selectDeck(decks[deck].title)}
+          key={decks[deck].title}
+          style={styles.deckCard}
+        >
+          <Text style={{ fontSize: 20 }}>{decks[deck].title}</Text>
+          <Text>{decks[deck].questions.length} cards</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+  deckCard: {
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 5,
+  },
+});
