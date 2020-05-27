d06645c8e3dfc1c9aed2bc476eead87004a936b4
Daniel Cancado Caram Andre <dandre9@gmail.com>
2020-05-26 23:59:49 -0300

    feat: Add navigation

--- App.js
+++ App.js
@@ -1,16 +1,70 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import DeckListView from "./components/DeckListView";
+import NewDeckView from "./components/NewDeckView";
+import DeckView from "./components/DeckView";
+import NewQuestionView from "./components/NewQuestionView";
+import QuizView from "./components/QuizView";
+import { saveDeckTitle, addCardToDeck, getDecks } from "./utils/api";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  state = {
+    decks: {},
+    selectedDeck: "",
+  };
+
+  componentDidMount() {
+    getDecks().then((response) => {
+      this.setState({ decks: response });
+    });
+  }
+
+  createDeck = (deckTitle) => {
+    const { decks } = this.state;
+
+    if (typeof decks[deckTitle] === "undefined") {
+      const newDeck = saveDeckTitle(deckTitle);
+
+      decks[deckTitle] = newDeck;
+
+      this.setState({ decks });
+    }
+  };
+
+  createQuestion = (question) => {
+    let { decks, selectedDeck } = this.state;
+
+    addCardToDeck(selectedDeck, question);
+    decks[selectedDeck].questions.push(question);
+
+    this.setState({ decks });
+  };
+
+  selectDeck = (deckTitle) => {
+    this.setState({ selectedDeck: deckTitle });
+  };
+
+  render() {
+    return (
+      <NavigationContainer
+        style={{
+          flex: 1,
+          padding: 20,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Stack.Navigator>
+          <Stack.Screen name="Decks" component={DeckListView} />
+          <Stack.Screen name="New Deck" component={NewDeckView} />
+          <Stack.Screen name="Deck" component={DeckView} />
+          <Stack.Screen name="New Question" component={NewQuestionView} />
+          <Stack.Screen name="Quiz" component={QuizView} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
