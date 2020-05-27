8f4f83741a847918c4bb780c4cfb5a7f71f867cf
Daniel Cancado Caram Andre <dandre9@gmail.com>
2020-05-27 02:13:05 -0300

    feat: Change fucntions to async and add reminder to notification

--- utils/api.js
+++ utils/api.js
@@ -1,22 +1,41 @@
 import { AsyncStorage } from "react-native";
 
-const FLASHCARDS_STORAGE_KEY = "MobileFlashcards:decks";
+const FLASHCARDS_STORAGE_KEY = "MobileFlashcards:data";
+const NOTIFICATION_REMINDER = "MobileFlashcards:reminder";
 
-export function getDecks() {
-  return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY);
+export async function getReminder() {
+  let reminder = await AsyncStorage.getItem(NOTIFICATION_REMINDER);
+
+  return reminder;
 }
 
-// export function getDeck(title){
-//     return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY);
-// }
+export async function saveReminder() {
+  const date = new Date().setHours(0, 0, 0, 0);
 
-export function saveDeckTitle(title) {
+  await AsyncStorage.setItem(NOTIFICATION_REMINDER, date.toString());
+}
+
+export async function getDecks() {
+  let decks = await AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY);
+
+  return decks ? JSON.parse(decks) : {};
+}
+
+export async function getDeck(title) {
+  const response = await AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY);
+  const decks = JSON.parse(response);
+  const deck = decks[title];
+
+  return deck;
+}
+
+export async function saveDeckTitle(title) {
   const deck = {
     title: title,
     questions: [],
   };
 
-  AsyncStorage.mergeItem(
+  await AsyncStorage.mergeItem(
     FLASHCARDS_STORAGE_KEY,
     JSON.stringify({
       [title]: deck,
@@ -26,22 +45,18 @@ export function saveDeckTitle(title) {
   return deck;
 }
 
-export function addCardToDeck(title, card) {
-  const results = AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY);
+export async function addCardToDeck(title, card) {
+  const response = await AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY);
+  const decks = JSON.parse(response);
+  const deck = decks[title];
+  deck.questions = deck.questions.concat([card]);
 
-  if (results) {
-    const data = JSON.parse(results);
-    const deck = data[title];
+  AsyncStorage.mergeItem(
+    FLASHCARDS_STORAGE_KEY,
+    JSON.stringify({
+      [title]: deck,
+    })
+  );
 
-    deck.questions = deck.questions.concat([card]);
-
-    AsyncStorage.mergeItem(
-      FLASHCARDS_STORAGE_KEY,
-      JSON.stringify({
-        [title]: deck,
-      })
-    );
-
-    return card;
-  }
+  return deck;
 }
