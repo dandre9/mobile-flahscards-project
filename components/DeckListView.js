eb6330d5220f907e1b506592e8356d1e1695b541
Daniel Cancado Caram Andre <dandre9@gmail.com>
2020-05-27 02:08:01 -0300

    feat: Finalize component functionality

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,21 +1,75 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Alert,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { getDecks, getDeck, getReminder } from "../utils/api";
 
-export default function DeckListView({ decks, selectDeck }) {
-  return (
-    <View style={styles.container}>
-      {Object.keys(decks).map((deck) => (
+export default class DeckListView extends React.Component {
+  state = { decks: {} };
+
+  componentDidMount() {
+    getDecks().then((response) => {
+      this.setState({ decks: response });
+    });
+
+    getReminder().then((response) => {
+      if (!response) {
+        Alert.alert("Hey!", "You havent completed any quiz today.");
+      } else {
+        const today = new Date().setHours(0, 0, 0, 0);
+        if (today > parseInt(response, 10)) {
+          Alert.alert("Hey!", "You havent completed any quiz today.");
+        }
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    getDecks().then((response) => {
+      if (
+        Object.keys(this.state.decks).length !== Object.keys(response).length
+      ) {
+        this.setState({ decks: response });
+      }
+    });
+  }
+
+  selectDeck = (title) => {
+    getDeck(title).then((response) => {
+      this.props.navigation.navigate("Deck", { deck: response });
+    });
+  };
+
+  render() {
+    const { decks } = this.state;
+
+    return (
+      <ScrollView style={styles.container}>
+        {Object.keys(decks).map((deck) => (
+          <TouchableOpacity
+            onPress={() => this.selectDeck(decks[deck].title)}
+            key={decks[deck].title}
+            style={styles.deckCard}
+          >
+            <Text style={{ fontSize: 20 }}>{decks[deck].title}</Text>
+            <Text>{decks[deck].questions.length} cards</Text>
+          </TouchableOpacity>
+        ))}
         <TouchableOpacity
-          onPress={() => selectDeck(decks[deck].title)}
-          key={decks[deck].title}
-          style={styles.deckCard}
+          onPress={() => this.props.navigation.navigate("New Deck")}
+          style={styles.androidBtn}
         >
-          <Text style={{ fontSize: 20 }}>{decks[deck].title}</Text>
-          <Text>{decks[deck].questions.length} cards</Text>
+          <Text style={{ color: "white", textAlign: "center" }}>
+            + New deck
+          </Text>
         </TouchableOpacity>
-      ))}
-    </View>
-  );
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -28,5 +82,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 5,
+  },
+  androidBtn: {
+    marginTop: 25,
+    backgroundColor: "black",
+    padding: 10,
+    borderRadius: 5,
   },
 });
