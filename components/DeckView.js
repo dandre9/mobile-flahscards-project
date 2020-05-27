3d3e3705d482cd5ebf7b814294a9e84f9a5ac9a3
Daniel Cancado Caram Andre <dandre9@gmail.com>
2020-05-27 02:10:16 -0300

    feat: Insert funciotnality a update number of cards

--- components/DeckView.js
+++ components/DeckView.js
@@ -1,23 +1,46 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function DeckView({ selectedDeck, decks }) {
-  return (
-    <View style={styles.container}>
-      <Text>{selectedDeck}</Text>
-      {/* <Text>{decks[selectedDeck].questions.length} cards</Text> */}
-      <TouchableOpacity
-        style={[styles.androidBtn, { backgroundColor: "white" }]}
-      >
-        <Text style={[styles.btnText, { color: "black" }]}>Add Card</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.androidBtn, { backgroundColor: "black" }]}
-      >
-        <Text style={[styles.btnText, { color: "white" }]}>Start Quiz</Text>
-      </TouchableOpacity>
-    </View>
-  );
+export default class DeckView extends React.Component {
+  state = { deck: { questions: [] } };
+
+  componentDidMount() {
+    this.setState({ deck: this.props.route.params.deck });
+  }
+
+  componentDidUpdate() {
+    if (
+      this.props.route.params.deck.questions.length !==
+      this.state.deck.questions.length
+    ) {
+      this.setState({ deck: this.props.route.params.deck });
+    }
+  }
+
+  render() {
+    const { deck } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 20 }}>{deck.title}</Text>
+        <Text style={styles.countText}>{deck.questions.length} cards</Text>
+        <TouchableOpacity
+          onPress={() =>
+            this.props.navigation.navigate("New Question", { deck })
+          }
+          style={[styles.androidBtn, { backgroundColor: "white" }]}
+        >
+          <Text style={[styles.btnText, { color: "black" }]}>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("Quiz", { deck })}
+          style={[styles.androidBtn, { backgroundColor: "black" }]}
+        >
+          <Text style={[styles.btnText, { color: "white" }]}>Start Quiz</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -25,16 +48,14 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
   },
-  text: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 5,
+  countText: {
+    color: "gray",
+    marginTop: 5,
   },
   androidBtn: {
     marginTop: 15,
     padding: 10,
-    borderRadius: 2,
+    borderRadius: 5,
     borderWidth: 1,
   },
   btnText: {
